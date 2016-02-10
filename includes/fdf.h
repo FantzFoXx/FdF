69c05018b3edc0264773209348c79d4c2499875a
Ugo DELORME <udelorme@e3r11p6.42.fr>
2016-02-10 17:42:38 +0100

    pattern map data ok

--- includes/fdf.h
+++ includes/fdf.h
@@ -1,13 +1,22 @@
 #ifndef FDF_H
 # define FDF_H
 
-#define WIDTH 1920
-#define HEIGHT 1080
+# define WIDTH 1920
+# define HEIGHT 1080
+# define ABSOL(x) ((x) < 0 ? - (x) : (x))
 
 typedef struct	s_coord
 {
 	float		x;
 	float	y;
+	struct s_coord *right;
+	struct s_coord *down;
 }				t_coord;
+
+typedef struct	s_meta
+{
+	void *mlx;
+	void *wnd;
+}				t_meta;
 
 #endif
--- src/main.c
+++ src/main.c
@@ -4,88 +4,170 @@
 #include <math.h>
 #include "libft.h"
 #include <stdio.h>
+#include <stdlib.h>
 
-/*
-   void	trace_droite(void *mlx, void *wnd, t_coord point_a, t_coord point_b)
-   {
-   t_coord	index;
-
-   index.x = point_a.x;
-   index.y = point_a.y;
-   if (index.x != point_b.x || index.y != point_b.y)
-   {
-   index.x = floor((index.x / 2) + (point_b.x / 2));
-   index.y = floor((index.y / 2) + (point_b.y / 2));
-   printf("x : %f\n", index.x);
-   printf("y : %f\n", index.y);
-   mlx_pixel_put(mlx, wnd, (int)index.x, (int)index.y, 255);
-   ft_trace("rec_b", "pass");
-   trace_droite(mlx, wnd, point_a, index);
-   if (index.x != point_a.x || index.y != point_a.y)
-   {
-   ft_trace("rec_a", "pass");
-   trace_droite(mlx, wnd, index, point_b);
-   }
-   }
-   }
-   */
-
-void	trace_droite(void *mlx, void *wnd, t_coord point_a, t_coord point_b)
+t_coord		*t_coord_new(int x, int y)
 {
-	float a;
-	int i;
-	t_coord	index;
+	t_coord *new;
+
+	new = (t_coord *)malloc(sizeof(t_coord));
+	if (new)
+	{
+		new->x = x;
+		new->y = y;
+		new->right = NULL;
+		new->down = NULL;
+	}
+	return (new);
+}
+
+void		t_coord_push_right(t_coord **first, t_coord *new)
+{
+	t_coord *index;
+
+	index = *first;
+	if (!index)
+		*first = new;
+	else
+	{
+		while (index->right)
+			index = index->right;
+		index->right = new;
+	}
+}
+
+void		t_coord_push_down(t_coord **first, t_coord *new)
+{
+	t_coord *index;
+
+	index = *first;
+	if (!index)
+		*first = new;
+	else
+	{
+		while (index->down)
+			index = index->down;
+		index->down = new;
+	}
+}
+
+void	ligne(void *mlx, void *wnd, t_coord point_a, t_coord point_b)
+{
+	t_coord d_point;
+	t_coord index;
+	int	i;
+	int	xinc;
+	int	yinc;
+	int	cumul;
 
 	index.x = point_a.x;
 	index.y = point_a.y;
-	a = (point_b.y - point_a.y) / (point_b.x - point_a.x);
-
-	i = 0;
-	while (index.x < point_b.x)
+	d_point.x = point_b.x - point_a.x;
+	d_point.y = point_b.y - point_a.y;
+	xinc = ( d_point.x > 0 ) ? 1 : -1;
+	yinc = ( d_point.y > 0 ) ? 1 : -1;
+	d_point.x = ABSOL(d_point.x);
+	d_point.y = ABSOL(d_point.y);
+	mlx_pixel_put(mlx, wnd, index.x, index.y, 255);
+	if (d_point.x > d_point.y)
 	{
-		mlx_pixel_put(mlx, wnd, index.x, (a * i + point_a.y), 255);
-		index.x++;
-		i++;
+		cumul = d_point.x / 2;
+		for (i = 1 ; i <= d_point.x ; i++) 
+		{
+			index.x += xinc;
+			cumul += d_point.y;
+			if (cumul >= d_point.x) 
+			{
+				cumul -= d_point.x;
+				index.y += yinc; 
+			}
+			mlx_pixel_put(mlx, wnd, index.x, index.y, 255);
+		} 
 	}
-
+	else 
+	{
+		cumul = d_point.y / 2;
+		for (i = 1 ; i <= d_point.y ; i++) 
+		{
+			index.y += yinc;
+			cumul += d_point.x;
+			if (cumul >= d_point.y) 
+			{
+				cumul -= d_point.y;
+				index.x += xinc; 
+			}
+			mlx_pixel_put(mlx, wnd, index.x, index.y, 255);
+		}
+	}
 }
 
-void	call_trace(void *mlx, void *wnd, t_coord point_a, t_coord point_b)
+void		link_dots(t_coord *map)
 {
-	if (point_a.y < point_b.y)
-		trace_droite(mlx, wnd, point_b, point_a);
-	else
-		trace_droite(mlx, wnd, point_a, point_b);
+	if (map->right)
+	{
+		if (map->right->right)
+			link_dots(map->right);
+		if (map->down)
+		{
+			map->down->right = map->right->down;
+			link_dots(map->down);
+		}
+	}
+}
 
+t_coord		*create_map(t_coord *map, int x_dots, int y_dots, int space)
+{
+	t_coord *index;
+	int		y_bak;
+
+	y_bak = y_dots;
+	index = t_coord_new(0, 0);
+	map = index;
+	while (x_dots >= 0)
+	{
+		y_dots = y_bak;
+		while (y_dots >= 0)
+		{
+			t_coord_push_down(&index, t_coord_new(space * x_dots, space * y_dots));
+			y_dots--;
+		}
+		t_coord_push_right(&index, t_coord_new(space * x_dots, space * y_dots));
+		index = index->right;
+		x_dots--;
+	}
+	link_dots(map);
+	return (map);
+}
+
+void	print_map(t_meta env, t_coord *map)
+{
+	t_coord *index;
+	index = map;
+	while (map)
+	{
+		index = map;
+		while (index)
+		{
+			mlx_pixel_put(env.mlx, env.wnd, index->x, index->y, 255);
+			index = index->down;
+		}
+		map = map->right;
+	}
 }
 
 int		main(void)
 {
-	void	*mlx;
-	void	*wnd;
-	t_coord	point_a;
-	t_coord	point_b;
+	t_meta	env;
+	t_coord	begin;
+	t_coord *map;
 
-	point_a.x = 900;
-	point_a.y = 0;
-
-	point_b.x = 0;
-	point_b.y = 1050;
-
-	mlx = mlx_init();
-	wnd = mlx_new_window(mlx, WIDTH, HEIGHT, "Test 1");
-	mlx_pixel_put(mlx, wnd, point_a.x, point_a.y, 0x00FF0000);
-	mlx_pixel_put(mlx, wnd, point_a.x + 1, point_a.y + 1, 0x00FF0000);
-	mlx_pixel_put(mlx, wnd, point_a.x + 1, point_a.y, 0x00FF0000);
-	mlx_pixel_put(mlx, wnd, point_a.x, point_a.y + 1, 0x00FF0000);
-
-	mlx_pixel_put(mlx, wnd, point_b.x, point_b.y, 0x00FF0000);
-	mlx_pixel_put(mlx, wnd, point_b.x + 1, point_b.y + 1, 0x00FF0000);
-	mlx_pixel_put(mlx, wnd, point_b.x, point_b.y + 1, 0x00FF0000);
-	mlx_pixel_put(mlx, wnd, point_b.x + 1, point_b.y, 0x00FF0000);
-
-	call_trace(mlx, wnd, point_a, point_b);
-
-	mlx_loop(mlx);
+	map = NULL;
+	begin.x = 100;
+	begin.y = 100;
+	env.mlx = mlx_init();
+	env.wnd = mlx_new_window(env.mlx, WIDTH, HEIGHT, "Test 1");
+	map = create_map(map, 10, 10, 20);
+	print_map(env, map);
+	mlx_loop(env.mlx);
 	return (0);
 }
